dd9fce79ea5fd988d40f6cad8ce3d4502e21cbc4
Diplo2by <contactdarshanv@gmail.com>
2022-10-05 08:53:41 +0530

    writing contracts
    NFT.sol and NFTMarketplace.sol

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "freeown/node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "freeown/node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "freeown/node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle")
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
